bfc39a58873a3969d1091f132228ea3b6ab11e6e
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-05-25 12:53:00 -0300

    Configuracoes iniciais do projeto

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.get('/teste', (req, res) => res.status(200).send({ message: 'Boas vindas à api'}));
+
+app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`));
+
+module.exports = app;
