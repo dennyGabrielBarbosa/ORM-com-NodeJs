23720742ed3ac12ae229c2e66316ecd9a26a880b
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-05-26 15:18:54 -0300

    Adicionado rota get para pessoas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,7 @@
 const bodyParser = require('body-parser');
+const pessoas = require('./pessoasRoute.js')
 
 module.exports = app => {
     app.use(bodyParser.json());
-    app.get('/', (req, res) => {
-        res.send('olá');
-    });
+    app.use(pessoas);
 }
