b75cdc0ca68f4f5c87eb773178e65ce22ecb119d
DENNY GABRIEL BARBOSA DE MORAIS <dennygabriel@gmail.com>
2023-05-27 14:10:44 -0300

    Finalizado CRUD

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -25,6 +25,38 @@ class PessoaController {
         }
     }
 
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body;
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            res.status(201).json(novaPessoaCriada);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async atualizaPessoa(req, res) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id)}})
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id)}});
+            res.status(200).json(pessoaAtualizada);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async apagaPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id)}});
+            res.status(200).json({ mensagem: `id ${id} foi deletado com sucesso`});
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+
 }
 
 module.exports = PessoaController;
